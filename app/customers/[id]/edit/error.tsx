e1dec8871a4fe314c71fc8446914f3126d61a77a
Gaurav Rajpoot <gauravrajpoot@192.168.1.106>
2026-08-12 07:48:48 +0530

    Change customer edit error message to 'Something gone wrong'

--- app/customers/[id]/edit/error.tsx
+++ app/customers/[id]/edit/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
     <div className="p-6">
-      <p className="text-red-600">Something went wrong: {error.message}</p>
+      <p className="text-red-600">Something gone wrong: {error.message}</p>
       <button onClick={reset} className="mt-2 underline">Try again</button>
     </div>
   )
