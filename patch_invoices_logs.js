0f764b602ceb9af3b4e961c760165ac8ab5a1207
Gaurav Rajpoot <gauravrajpoot@192.168.1.83>
2026-08-20 17:21:34 +0530

    chore: cleanup patch script

--- patch_invoices_logs.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('app/actions/invoices.ts', 'utf8');
-code = code.replace(
-  'catch (err) {',
-  'catch (err) {\n    console.error("DEBUG ERROR:", err);'
-);
-fs.writeFileSync('app/actions/invoices.ts', code);
