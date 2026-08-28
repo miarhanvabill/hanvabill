dd2158d756780fab11d6a0a0490281f9eb9d1aba
Gaurav Rajpoot <gauravrajpoot@192.168.1.13>
2026-08-28 11:17:38 +0530

    chore: setup Sentry for error tracking

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from '@sentry/nextjs';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
@@ -14,4 +16,20 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+  org: "your-sentry-org",
+  project: "hanvabill",
+  
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+  
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
